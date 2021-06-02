82e5a3215c7570e18390327e7b742b20d50c7989
haidarfahmi2000@gmail.com <haidarfahmi2000@gmail.com>
2021-06-02 23:03:13 +0700

    Commit Logic for model

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask
+from flask import Flask, request
+from models.Preprocess import predict_image
+import cv2, numpy as np
+# from models.Preprocess import RequestToImage
 
 app = Flask(__name__)
 
@@ -8,8 +11,17 @@ def welcome():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    return "Hasil Predict"
+    # ini logic buat ambil gambar udah ini tugas gua
+    file = request.files['image'].read()
+    npimg = np.fromstring(file, np.uint8)
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+
+    model = tf.load('model.tflite') #ini logic yg dikasih temen gua untuk load model cmn masih belom bener kalian harus
+                                    #cari syntax benernya
+
+    return predict_image(img, model)
 
 if __name__ == "__main__":
     # app.run()
     app.run(debug=True, host='0.0.0.0', port='5000')
+
---
+++ models/Preprocess.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# ini logic udah benar tapi mungkin masih perlu perbaikan (karena ini belom tau bisa/engga) kalo engga harus diperbaiki
+def predict_image(image_uplo, model):
+    im = image_uplo
+    im_array = np.asarray(im)
+    im_array = im_array * (1 / 255)
+    im_input = tf.reshape(im_array, shape=[1, 224, 224, 3])
+
+    predict_array = model.predict(im_input)[0]
+
+    predict_label = np.argmax(model.predict(im_input))
+
+    if predict_label == 0:
+        print('covid')
+    elif predict_label == 1:
+        print('normal')
+    elif predict_label == 2:
+        print('penumonia')
