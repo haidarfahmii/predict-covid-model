bcb0c47ceaef2d6d1475348c902f6c607bd50322
philjaycee <67178498+philjaycee@users.noreply.github.com>
2021-06-05 10:13:00 +0700

    Update preprocess.py (rubah_1)

--- models/Preprocess.py
+++ models/Preprocess.py
@@ -1,10 +1,9 @@
 import numpy as np
 
 # ini logic udah benar tapi mungkin masih perlu perbaikan (karena ini belom tau bisa/engga) kalo engga harus diperbaiki
-def predict_label(img_path):
-    i = 
-
-    predict_array = model.predict(im_input)[0]
-
-    predict_label = np.argmax(model.predict(im_input))
-    return predict_label
+def predict_label(img2):
+    i = image.load_img(img2, target-size = (224,224))
+    i = iamge.img_to_array(i)/255.0
+    i = i.reshape(1,224,224,3)
+    p = model.predict_classes(i)
+    return dict[p[0]]
