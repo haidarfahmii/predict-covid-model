3952feeeaa32e986e977c537f38a3fda834c8c61
haidarfahmi2000@gmail.com <haidarfahmi2000@gmail.com>
2021-06-05 23:37:00 +0700

    Commit Final

--- main.py
+++ main.py
@@ -1,29 +1,42 @@
 from flask import Flask, request
-from keras.preprocessing import image
-from models.Preprocess import predict_image
 import cv2, numpy as np
-import tensorflow as tf 
-from keras.models import load_model
-# from models.Preprocess import RequestToImage
+import tensorflow as tf
 
 app = Flask(__name__)
 
+def predict_image(image_uplo, model):
+    im = image_uplo
+    im_array = np.asarray(im)
+    im_array = im_array * (1 / 255)
+    predict_label = np.argmax(model.predict(im_array))
 
-dict = { 0 : 'Covid', 1 : 'Normal, 2 : 'Pneumonia'}
+    if predict_label == 0:
+        return ('covid')
+    elif predict_label == 1:
+        return ('normal')
+    elif predict_label == 2:
+        return ('penumonia')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def welcome():
     return "Hello Flask!"
 
-model = load_model('model_ml_vg_1.h5')
-
 @app.route('/predict', methods=['POST'])
 def predict():
-    # ini logic buat ambil gambar udah ini tugas gua
+    # Load Model
+    model = tf.keras.models.load_model('model_ml_vg_1.h5')
+
     file = request.files['image'].read()
     npimg = np.fromstring(file, np.uint8)
-    img2 = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
-    return predict_image(img2)
+    img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+    img = cv2.resize(img, (224, 224))
+
+    x = tf.keras.preprocessing.image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    img = np.vstack([x])
+
+    return predict_image(img, model)
 
 if __name__ == "__main__":
     # app.run()
--- models/Preprocess.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# ini logic udah benar tapi mungkin masih perlu perbaikan (karena ini belom tau bisa/engga) kalo engga harus diperbaiki
-def predict_image(img2):
-    i = image.load_img(img2, target-size = (224,224))
-    i = iamge.img_to_array(i)/255.0
-    i = i.reshape(1,224,224,3)
-    p = model.predict_classes(i)
-    return dict[p[0]]
