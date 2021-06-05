7036e5b4daac2672fb0dfe52d2aa390c6dbe4dd4
philjaycee <67178498+philjaycee@users.noreply.github.com>
2021-06-05 10:14:22 +0700

    Update Preprocess.py

--- models/Preprocess.py
+++ models/Preprocess.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # ini logic udah benar tapi mungkin masih perlu perbaikan (karena ini belom tau bisa/engga) kalo engga harus diperbaiki
-def predict_label(img2):
+def predict_image(img2):
     i = image.load_img(img2, target-size = (224,224))
     i = iamge.img_to_array(i)/255.0
     i = i.reshape(1,224,224,3)
