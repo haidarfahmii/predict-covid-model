6f0e09c216a799895565f5087523e0f0969c7100
philjaycee <67178498+philjaycee@users.noreply.github.com>
2021-06-05 10:04:00 +0700

    Update Preprocess.py

--- models/Preprocess.py
+++ models/Preprocess.py
@@ -1,11 +1,8 @@
 import numpy as np
 
 # ini logic udah benar tapi mungkin masih perlu perbaikan (karena ini belom tau bisa/engga) kalo engga harus diperbaiki
-def predict_image(image_uplo, model):
-    im = image_uplo
-    im_array = np.asarray(im)
-    im_array = im_array * (1 / 255)
-    im_input = tf.reshape(im_array, shape=[1, 224, 224, 3])
+def predict_label(img_path):
+    i = 
 
     predict_array = model.predict(im_input)[0]
 
