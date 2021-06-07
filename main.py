4e58c756b10a73fa03681d507537e646d2754dca
haidarfahmi2000@gmail.com <haidarfahmi2000@gmail.com>
2021-06-07 14:19:46 +0700

    Commit Final v.1

--- main.py
+++ main.py
@@ -41,6 +41,5 @@ def predict():
 
 if __name__ == "__main__":
     # app.run()
-    # app.run(debug=True, host='0.0.0.0', port='5000')
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port='5000')
 
